74944db550921b4658bead6866b8bc3704a35148
ZeroPR <rjpt93@gmail.com>
2018-10-19 10:23:17 -0400

    New

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in menuItems"
+          :key="i"
+          :to="item.to"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon v-html="miniVariant ? 'fas fa-chevron-right' : 'fas fa-chevron-left'"></v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-tooltip bottom>
+        <v-btn icon
+        slot="activator">
+          <v-icon>fas fa-power-off</v-icon>
+        </v-btn>
+        <span>Cerrar Sesion</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <v-btn icon
+        slot="activator">
+          <v-icon>fas fa-ellipsis-v</v-icon>
+        </v-btn>
+        <span>Menu</span>
+      </v-tooltip>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      drawer: true,
+      menuItems: [{
+        icon: 'fas fa-home',
+        title: 'Inspire',
+        to: '/',
+      },
+      {icon: 'fab fa-wpforms', title: 'Registrar', to: '/registrar'},
+      {icon: 'fas fa-clipboard-list', title: 'Registro', to: '/registros'},
+      {icon: 'fas fa-info-circle', title: 'About', to: '/about'},
+      ],
+      miniVariant: false,
+      title: 'Vuetify.js'
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/axios'
+import './plugins/vuetify'
 import App from './App.vue'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import router from './router'
+import store from './store'
+import VueSession from 'vue-session'
 
 Vue.config.productionTip = false
 
+Vue.use(VueSession)
+
 new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
